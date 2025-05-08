2ab84c0e518f82832f7529e0d0900d5d4424a17f
saliha-ziyard <salihaziyard@gmail.com>
2025-05-08 16:01:49 +0530

    fourth component updated

--- frontend/src/components/ui/home/Forth.jsx
+++ frontend/src/components/ui/home/Forth.jsx
@@ -1,39 +1,62 @@
-'use client'
+"use client";
 import Link from "next/link";
 import RaqisCard from "@/components/cards/RaqisCard";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import ResponsiveGrid from "@/components/ui/layout/ResponsiveGrid";
 
 function Forth(props) {
-    const { raqiData = [], title } = props;
-    // const slice = raqiData?.slice(0, 4) || [];
+  const { raqiData = [], title } = props;
 
   if (!raqiData) {
     return null;
   }
 
   return (
-    <div id="Forth" className={`flex flex-col  md:mx-[7%] lg:mx-[7%] mt-20 m-5 rounded-lg ${[props.className]}  animate-fade-in`} style={{ animationDelay: '0.1s' }}>
-      <div className="flex justify-between items-center animate-fade-in" style={{ animationDelay: '0.2s' }}>
-        <h1 className="text-xl md:text-3xl font-bold text-RuqyaGray">{title}</h1>
-        {raqiData.length > 3 && (
-          <div className="text-center animate-fade-in" style={{ animationDelay: '0.3s' }}>
-            <Link href="/BookRaqis" className="text-RuqyaGreen font-bold">
-              See all <FaLongArrowAltRight className="inline mb-1" />
-            </Link>
-          </div>
-        )}
-      </div>
+    <div
+      id="Forth"
+      className={`mt-20 animate-fade-in md:mx-[7%] lg:mx-[7%] ${props.className} mb-20`}
+      style={{ animationDelay: "0.1s" }}
+    >
+      {/* Same container padding/margins as 'First' */}
+      <div className="relative max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div
+          className="flex justify-between items-center animate-fade-in"
+          style={{ animationDelay: "0.2s" }}
+        >
+          <h1 className="text-xl md:text-3xl font-bold text-RuqyaGray">
+            {title}
+          </h1>
+          {raqiData.length > 3 && (
+            <div
+              className="text-center animate-fade-in"
+              style={{ animationDelay: "0.3s" }}
+            >
+              <Link href="/BookRaqis" className="text-RuqyaGreen font-bold">
+                See all <FaLongArrowAltRight className="inline mb-1" />
+              </Link>
+            </div>
+          )}
+        </div>
 
-      {/* <Grid>
-        {raqiData && slice.map((data) => (
-          <RaqisCard key={data.id} raqi={data} />
-        ))}
-        </Grid> */}
-
-      <ResponsiveGrid data={raqiData} breakpoints={{mobile:4, ipad: 4, 'ipad-landscape': 4, lg:3, xl: 3, '2xl': 3, '3xl': 5, '4xl': 6, '5xl': 6 }} className="animate-fade-in" style={{ animationDelay: '0.4s' }}>
-          {(data) => <RaqisCard key={`Raqi` + data.id} raqi={data} />}
+        <ResponsiveGrid
+          data={raqiData}
+          breakpoints={{
+            mobile: 4,
+            ipad: 4,
+            "ipad-landscape": 4,
+            lg: 3,
+            xl: 3,
+            "2xl": 3,
+            "3xl": 5,
+            "4xl": 6,
+            "5xl": 6,
+          }}
+          className="animate-fade-in mt-8"
+          style={{ animationDelay: "0.4s" }}
+        >
+          {(data) => <RaqisCard key={`Raqi${data.id}`} raqi={data} />}
         </ResponsiveGrid>
+      </div>
     </div>
   );
 }
