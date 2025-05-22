02bcc44000ae2ab38bc7dabeed8660f9d2166837
saliha-ziyard <salihaziyard@gmail.com>
2025-05-22 23:17:36 +0530

    second component additional point added

--- frontend/src/components/ui/home/Second.jsx
+++ frontend/src/components/ui/home/Second.jsx
@@ -131,7 +131,13 @@ function Second() {
                     <svg className="w-5 h-5 text-RuqyaLightGreen mr-2" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                     </svg>
-                    <span>1-on-1 Online Consultation or real-time chat</span>
+                    <span>1-on-1 Online Consultation</span>
+                  </div>
+                  <div className="flex items-center px-4 py-2 bg-RuqyaLightPurple/30 rounded-full">
+                    <svg className="w-5 h-5 text-RuqyaLightGreen mr-2" fill="currentColor" viewBox="0 0 20 20">
+                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                    </svg>
+                    <span>Start a real-time chat with a trusted Muabbir</span>
                   </div>
                   <div className="flex items-center px-4 py-2 bg-RuqyaLightPurple/30 rounded-full">
                     <svg className="w-5 h-5 text-RuqyaLightGreen mr-2" fill="currentColor" viewBox="0 0 20 20">
