d317efc942321ba9e0e7d4b3334255b770e0b1e9
saliha-ziyard <salihaziyard@gmail.com>
2025-05-23 16:31:29 +0530

    faq animation added

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -43,7 +43,7 @@ export default function Home() {
       <Second className="animate-fade-in" style={{ animationDelay: '0.4s' }} />
       {myBookings && myBookings.length !== 0 && <Third raqiData={myBookings} className="animate-fade-in" style={{ animationDelay: '0.5s' }} />}
       {raqiData && <Forth  raqiData={raqiData} title="Meet Our Expert Muabbirs" className=" animate-fade-in" style={{ animationDelay: '0.6s' }} />}
-      <Fifth />
+      <Fifth className="animate-fade-in" style={{ animationDelay: '0.6s' }}/>
     </div>
   );
 }
